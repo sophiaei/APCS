da6d82cfee7fe3515d8afa0d9c4ddb0ebdeeae26
sophiaei <seiden30@stuy.edu>
2021-10-04 15:23:13 -0400

    added 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+//Sophia Eiden
+//APCS
+//HW10 -- Refactor Big Sib One -- methods with return statements
+//2021 - 10 - 04
+/*
+DISCOVERIES
+ - return w/ return statement. unnecessary to use print unless otherwise intended.
+UNRESOLVED QUESTIONS
+- n/a
+*/
+
+public class BigSib {
+  public static String greet(String name){
+    return("Good day, " + name+". Fare you well?");
+  }
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,18 @@
+//Sophia Eiden
+//APCS
+//HW10 -- Refactor Big Sib One -- methods with return statements
+//2021 - 10 - 04
+/*
+DISCOVERIES
+ - return w/ return statement. unnecessary to use print unless otherwise intended.
+UNRESOLVED QUESTIONS
+- n/a
+*/
+
+public class Greet{
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
