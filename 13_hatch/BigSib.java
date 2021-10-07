051587a028dd9995e413ccc7121be9dc4323b604
sophiaei <seiden30@stuy.edu>
2021-10-06 22:26:57 -0400

    revisions

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,12 +1,15 @@
-//Sophia Eiden
+//Team Elder Gods: Sophia Eiden, Prattay Dey
 //APCS
-//HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
-//2021-10-5
+//HW13 -- Where do BigSibs Come From? / Constructors
+//2021-10-6
 /*
-DISCO
-- constructors can have variable defs inside them
-QCC
+DISCO:
+- Constructors can have variable defs inside them
+- You can call on the constructor to create an object in the class, using keyword "new"
+- Constructors can have multiple parameters
 
+QCC:
+- What are the benefits of using a constructor compared to a method?
 */
 
 public class BigSib {
--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,7 +1,16 @@
-//Sophia Eiden
+//Team Elder Gods: Sophia Eiden, Prattay Dey
 //APCS
-//HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
-//2021-10-5
+//HW13 -- Where do BigSibs Come From? / Constructors
+//2021-10-6
+/*
+DISCO:
+- Constructors can have variable defs inside them
+- You can call on the constructor to create an object in the class, using keyword "new"
+- Constructors can have multiple parameters
+
+QCC:
+- What are the benefits of using a constructor compared to a method?
+*/
 
 public class Greet {
   public static void main( String[] args ) {
