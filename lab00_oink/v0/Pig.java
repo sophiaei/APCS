706f9440109624a4cb2dfe9b66f3f01dc53fc4a9
sophiaei <seiden30@stuy.edu>
2021-11-08 22:14:04 -0500

    init setup

--- lab00_oink/v0/Pig.java
+++ lab00_oink/v0/Pig.java
@@ -1,10 +1,31 @@
+/***
+ * Clyde "Thluffy" Sinclair
+ * APCS
+ * HW31 -- deploying linear search on Strings, using helper methods
+ * 2021-11-04r
+ * time spent: .5hrs
+ *
+ * class Pig
+ * a Pig Latin translator
+ * ~~~~~~~~~~~~~~~~~~~ SKELETON ~~~~~~~~~~~~~~~~~~~
+ *           9
+ *     ,--.-'-,--.
+ *     \  /-~-\  /
+ *    / )' a a `( \
+ *   ( (  ,---.  ) )
+ *    \ `(_o_o_)' /
+ *     \   `-'   /
+ *      | |---| |
+ *      [_]   [_]
+ *      PROTIP: Make this class compilable first,
+ *      then develop and test one method at a time.
+ *      NEVER STRAY TOO FAR FROM COMPILABILITY/RUNNABILITY!
+ ***/
 
 
 public class Pig {
-  private static final String VOWELS = "aeiouy";
-  private static final String CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  private static final String PUNCS = ".,:;!?";
 
+  private static final String VOWELS = "aeiou";
 
   /**
     boolean hasA(String,String) -- checks for a letter in a String
@@ -18,7 +39,9 @@ public class Pig {
 
     /* equiv code, wo using indexOf()...
        boolean ans = false;
+
        for( int i = 0; i < w.length(); i++ ) {
+
        if ( w.substring(i,i+1).equals(letter) ) {
        ans = true;
        //Q: is there a more efficient way?
@@ -51,6 +74,7 @@ public class Pig {
        int numVowels = 0; //init vowels counter var
        //must touch each letter in word, so use FOR
        for( int i = 0; i < w.length(); i++ ) {
+
        if ( isAVowel( w.substring(i,i+1) ) )
        numVowels++;
        }
@@ -65,11 +89,18 @@ public class Pig {
     post: hasAVowel("cat") -> true
     hasAVowel("zzz") -> false
     **/
-  public static boolean hasAVowel( String w ) {
-    return w.indexOf(w) >= 0;
-  }
+  public static boolean hasAVowel( String w ){
+     for (int i = 0; i < w.length(); i++){
+       for (int j = 0; j < VOWELS.length(); j ++){
+         if (w.substring(i,i+1).equals(VOWELS.substring(j,j+1))){
+           return true;
+         }
+       }
+     }
 
-
+     return false;
+     /* YOUR IMPLEMENTATION HERE */
+   }
   /**
     String allVowels(String) -- returns vowels in a String
     pre:  w != null
@@ -115,48 +146,6 @@ public class Pig {
   public static boolean beginsWithVowel( String w ) {
     return isAVowel( w.substring(0,1) );
   }
-
-  /*=====================================
-    boolean isPunc(String) -- tells whether a character is punctuation
-    pre:  symbol.length() == 1
-    post: isPunc(".") -> true
-          isPunc("b") -> false
-    =====================================*/
-  public static boolean isPunc( String symbol ) {
-    return PUNCS.indexOf( symbol ) != -1;
-  }
-  /*=====================================
-    boolean isUpperCase(String) -- tells whether a letter is uppercase
-    pre:  letter.length() == 1
-    post: isUpperCase("a") -> false
-          isUpperCase("A") -> true
-    =====================================*/
-  public static boolean isUpperCase( String letter ) {
-    return CAPS.indexOf(letter) != -1;
-   }
-
-   /*=====================================
-     boolean hasPunc(String) -- tells whether a String contains punctuation
-     pre:  w != null
-     post: hasPunc(“cat.”) -> true
-           hasPunc(“cat”) -> false
-     =====================================*/
-   public static boolean hasPunc( String w ) {
-     return
-   }
-
-
-   /*=====================================
-     boolean beginsWithUpper(String) -- tells whether 1st letter is uppercase
-     pre:  w != null and w.length() > 0
-     post: beginsWithUpper("Apple") -> true
-           beginsWithUpper("apple") -> false
-     =====================================*/
-   public static boolean beginsWithUpper( String w ) {
-
-     return isUpperCase(w.substring(0,1) );
-   }
-
 
 
   /**
