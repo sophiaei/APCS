98b873055757fc953744fc5a69ae1efe3818baa3
sophiaei <seiden30@stuy.edu>
2022-02-16 12:02:42 -0500

    65

---
+++ 65_nqueens/QueenBoard.java
@@ -0,0 +1,185 @@
+/***
+ * class QueenBoard
+ * Generates solutions for N-Queens problem.
+ * USAGE:
+ * 1. Peruse. Transcribe your KtS verbiage into block comments preceding each method where necessary.
+ * 2. Implement solver method.
+ */
+
+public class QueenBoard
+{
+
+  private int[][] _board;
+
+  public QueenBoard( int size )
+  {
+    _board = new int[size][size];
+  }
+
+
+  /***
+   * precondition: board is filled with 0's only.
+   * postcondition:
+   * If a solution is found, board shows position of N queens,
+   * returns true.
+   * If no solution, board is filled with 0's,
+   * returns false.
+   */
+  public boolean solve()
+  {
+    n = 0;
+    if (solveH(n)){
+      return true;
+    }
+
+    return false;
+  }
+
+
+  /**
+   *Helper method for solve.
+   */
+  private boolean solveH( int col )
+  {
+    if (col == _board.length){
+      return true;
+    }
+    for (int i =0; i < _board.length; i++){
+      if (addQueen(i,col)){
+        return solveH(col+1);
+      }
+      else{
+        for (int j = 0; j < _board.length; j++){
+          if (removeQueen(j, col -1)){
+            return solveH(col-1);
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+
+  public void printSolution()
+  {
+    /** Print board, a la toString...
+        Except:
+        all negs and 0's replaced with underscore
+        all 1's replaced with 'Q'
+    */
+  }
+
+
+
+  //================= YE OLDE SEPARATOR =================
+
+  /***
+   * places a queen at (row,column), and marks all the squares to the right that become attackable
+   * precondition: row and column are valid array indices
+   * postcondition: If the chosen square is attackable/invalid, return false
+                    Else (the queen has been placed) return true
+   */
+  private boolean addQueen(int row, int col)
+  {
+    if(_board[row][col] != 0){
+      return false;
+    }
+    _board[row][col] = 1;
+    int offset = 1;
+    while(col+offset < _board[row].length){
+      _board[row][col+offset]--;
+      if(row - offset >= 0){
+        _board[row-offset][col+offset]--;
+      }
+      if(row + offset < _board.length){
+        _board[row+offset][col+offset]--;
+      }
+      offset++;
+    }
+    return true;
+  }
+
+
+  /***
+   * removes the queen at (row,column), and marks all the previously attackable squares to the right as neutral
+   * precondition: row and column are valid array indices
+   * postcondition: If the chosen square has no queen to remove, return false
+                    Else (the queen has been removed) return true
+   */
+  private boolean removeQueen(int row, int col){
+    if ( _board[row][col] != 1 ) {
+      return false;
+    }
+    _board[row][col] = 0;
+    int offset = 1;
+
+    while( col+offset < _board[row].length ) {
+      _board[row][col+offset]++;
+      if( row - offset >= 0 ) {
+        _board[row-offset][col+offset]++;
+      }
+      if( row + offset < _board.length ) {
+        _board[row+offset][col+offset]++;
+      }
+      offset++;
+    }
+    return true;
+  }
+
+
+  /***
+   *returns the board in string format
+   * precondition: valid board of integer
+   * postcondition: nxn board with all integer representations
+   */
+  public String  toString()
+  {
+    String ans = "";
+    for( int r = 0; r < _board.length; r++ ) {
+      for( int c = 0; c < _board[0].length; c++ ) {
+        ans += _board[r][c]+"\t";
+      }
+      ans += "\n";
+    }
+    return ans;
+  }
+
+
+  //main method for testing...
+  public static void main( String[] args )
+  {
+    QueenBoard b = new QueenBoard(5);
+    System.out.println(b);
+    /** should be...
+       0	0	0	0	0
+       0	0	0	0	0
+       0	0	0	0	0
+       0	0	0	0	0
+       0	0	0	0	0
+    */
+
+    b.addQueen(3,0);
+    b.addQueen(0,1);
+    System.out.println(b);
+    /** should be...
+       0	1	-1	-2	-1
+       0	0	-2	0	0
+       0	-1	0	-1	0
+       1	-1	-1	-1	-2
+       0	-1	0	0	0
+    */
+
+    b.removeQueen(3,0);
+    System.out.println(b);
+    /** should be...
+       0	1	-1	-1	-1
+       0	0	-1	0	0
+       0	0	0	-1	0
+       0	0	0	0	-1
+       0	0	0	0	0
+    */
+
+    b.solve();
+  }
+
+}//end class
