f24fcd81dc4d2fa3ef97511a305d04ffcf8e7535
sophiaei <seiden30@stuy.edu>
2022-03-08 10:22:14 -0500

    71

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -1,65 +1,166 @@
-// SCH: David, Sophia, Jing
-// APCS pd06
-// HW71 - Whut Dis.
-// 2022-03-07
-// time spent: 30mins
-/*
-As an initial starting point, I transcribed the pseudo-code from the paper to
-workable java code.
+// Submarine Cable History: Sophia Eiden, Jing Yi Feng, David Deng
+// APCS pd6
+// HW71 -- Reading for intent, tracing for VICTORY
+// 2022-03-07m
+// time spent: 0.3 hrs
 
-Interesting! With reverse sorted and sorted configurations of the array,
-  it returns a sorted array.
-  Perhaps sorting is something related to it?
+/***
+ * SKEELTON for
+ * class Mysterion
+ * tests preliminary hypotheses re: utility of mystery algo
+ *
+ * <h1>It's a Bird, It's a Plane! It's.... MYSTERION</h1>
+ * The Mysterion method ... <YOUR TRIO'S DESCRIPTION HERE>
+ * <p>
+ * <b>Note:</b> BLAH blah blah, yakkety schmakketty...
+ * @author  Trey Parker, Matt Stone
+ * @version 3.1415926535897932384626433
+ * @since   2022-03-07m
+ *
+ * algo as pseudocode:
+ * ------------------------------
+ *     v = arr[c]
+ *     swap arr[c], arr[b]
+ *     s = a
+ *     for i in [a..b-1]
+ *         if arr[i] < v
+ *             swap arr[s], arr[i]
+ *             s+=1
+ *     swap arr[b], arr[s]
+ *
+ * DISCO
+ * Organizes elements based on its relationship to a single number in the subarray.
+ * QCC
+ * Similar to merge sort? Divide array into smaller subarrays whilst organizing.
+ * q0: What does it do?
+ * a0: Iterates through subarray and categorizes elements in one of either two options:
+ * left of index (smaller than the element at the partition of the original array)
+ * right of index (greater than the element at the partition of the original array)
+ *
+ * q1: O(?)
+ * a1: O(n) because it iterates through the subarray once.
+ *
+ ***/
 
-It always gets at least close to sorted, with one or two spots wrong.
-No idea where to go from here - also too late.
-*/
 
-import java.util.Arrays;
-public class Mysterion{
+public class Mysterion
+{
+  //--------------v  HELPER METHODS  v--------------
+  //swap values at indices x, y in array o
+  public static void swap( int x, int y, int[] o )
+  {
+    int tmp = o[x];
+    o[x] = o[y];
+    o[y] = tmp;
+  }
 
-  public static int[] unknown(int[] arr, int a, int b, int c) {
-    int v = arr[c];
-    //swap arr[c], arr[b]
-    int temp = arr[c];
-    arr[c] = arr[b];
-    arr[b] = temp;
-    //
-    int s = a;
-    for (int i = 0; i < b; i++){
-      if (arr[i] < v){
-        //swap arr[s], arr[i]
-        temp = arr[s];
-        arr[s] = arr[i];
-        arr[i] = temp;
-        //
-        s+=1;
-      }
+  //print input array
+  public static void printArr( int[] a )
+  {
+    for ( int o : a )
+      System.out.print( o + " " );
+    System.out.println();
+  }
+
+  //shuffle elements of input array
+  public static void shuffle( int[] d )
+  {
+    int tmp;
+    int swapPos;
+    for( int i = 0; i < d.length; i++ ) {
+      tmp = d[i];
+      swapPos = i + (int)( (d.length - i) * Math.random() );
+      swap( i, swapPos, d );
     }
-    //swap arr[b], arr[s]
-    temp = arr[b];
-    arr[b] = arr[s];
-    arr[s] = temp;
-
-    return arr;
   }
 
-  public static void main(String[] args) {
-    int[] array = {7,1,5,12,3};
-
-    System.out.println(Arrays.toString(unknown(array, 0,4,2)));
-
-    int[] array2 = {12,7,5,3,1};
-
-    System.out.println(Arrays.toString(unknown(array2, 0,4,2)));
-
-    int[] array3 = {1,3,5,7,12};
-
-    System.out.println(Arrays.toString(unknown(array3, 0,4,2)));
-
-    int[] array4 = {5,3,1,12,7};
-
-    System.out.println(Arrays.toString(unknown(array4, 0,4,2)));
-
+  //return int array of size s, with each element fr range [0,maxVal)
+  public static int[] buildArray( int s, int maxVal )
+  {
+    int[] retArr = new int[s];
+    for( int i = 0; i < retArr.length; i++ )
+      retArr[i] = (int)( maxVal * Math.random() );
+    return retArr;
   }
-}
+  //--------------^  HELPER METHODS  ^--------------
+
+
+  /**
+   * int mysterion(int[],int,int,int)
+   * DESCRIP
+   *
+   * @param arr
+   * @param a
+   * @param b
+   * @param c
+   * @return int
+   *
+   */
+  public static int mysterion( int arr[], int l, int h, int pIndex)
+  {
+	int partition = arr[pIndex];
+
+    swap(pIndex, h, arr);
+
+    int s = l;
+    for (int i = l; i < h - 1; i++) {
+        if (arr[i] < partition) {
+            swap(s, i, arr);
+            s++;
+        }
+    }
+    swap(h, s, arr);
+    return s;
+  }//end mysterion
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+    //init test arrays of magic numbers
+    int[] arr1 = {8,21,17,69,343};
+    int[] arr3 = {1,28,33,4982,37};
+    int[] arr4 = {5,4,17,9000,6};
+    int[] arr5 = {3,0,16,599,1024};
+
+
+    // run mysterion on each array,
+    // holding a & b fixed, varying c...
+    for( int testC = 0; testC < 5; testC++ ) {
+    System.out.println("arr1: ");
+    printArr(arr1);
+    mysterion(arr1,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr1);
+    System.out.println("-----------------------");
+
+    System.out.println("arr3:");
+    printArr(arr3);
+    mysterion(arr3,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr3);
+    System.out.println("-----------------------");
+
+    System.out.println("arr4:");
+    printArr(arr4);
+    mysterion(arr4,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr4);
+    System.out.println("-----------------------");
+
+    System.out.println("arr5:");
+    printArr(arr5);
+    mysterion(arr5,0,4,testC);
+    System.out.println("after mysterion w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr5);
+    System.out.println("-----------------------");
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }
+  }//end main
+}//end class Mysterion
