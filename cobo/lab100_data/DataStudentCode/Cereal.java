69fa5199ff4c4510b8e32a3d563b2db8b31bbaf7
sophiaei <seiden30@stuy.edu>
2022-04-05 12:34:45 -0400

    lab08

---
+++ cobo/lab100_data/DataStudentCode/Cereal.java
@@ -0,0 +1,15 @@
+
+public class Cereal{
+  public String name;
+  public char type;
+  public int calories, protein, fats,sodium, fiber, carbs, sugar, potassium, vitamins, shelf;
+  public float cups, weight, rating;
+
+  
+
+  // TOSTRING? Acessors
+
+  //
+
+
+}
